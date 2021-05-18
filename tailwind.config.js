b0f615321e6cd9151282bf9f2c9925efe2e23c58
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-05-18 21:45:32 +0700

    Landing page Part 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   purge: ["./public/**/*.html"],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors:{
+      'orange':'#FF7143',
+      'darkblue':'#5454D4',
+      'black': '#19191B',
+      'gray' : '#696871',
+      'red' : '#696871',
+      'white': '#FFFFFF',
+    },
     extend: {},
   },
   variants: {},
